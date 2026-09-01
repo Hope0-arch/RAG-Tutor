11b02b75d8d7e7a8bef4e2802129f356fb618777
Jayaram.N.Ayer <demigod4854@gmail.com>
2026-09-02 00:18:28 +0530

    Update rag_algorithm.py
    Added a function to load the chunks of the source instead of re-initializing the process each run

--- rag_algorithm.py
+++ rag_algorithm.py
@@ -1,74 +1,3 @@
-"""import ollama as olm
-import numpy as np
-from pypdf import PdfReader
-from pathlib import Path
-
-# Takes the text of the pdf and creates chunks for embedding in a way where data is not lost
-def chunker(text,chunk_size=180,stepback=30):
-    words=text.split()
-    chunk_list=[]
-    step= chunk_size-stepback # This is the line used to give some of the words of the previous line to the new one for continuity
-    for start in range(0,len(words),step):
-        chunk_words= words[start:start+chunk_size]
-        chunk=" ".join(chunk_words)
-        chunk_list.append(chunk)
-    return chunk_list
-
-def reader(pdf_path):
-    record=PdfReader(pdf_path)
-
-    records=[]
-
-    for page_number,page in enumerate(record.pages,start=1):
-        text=page.extract_text() or ""
-        page_chunks= chunker(text)
-        for chunks in page_chunks:
-            records.append(
-            {
-                "text":chunks,
-                "page number":page_number,
-                "source":Path(pdf_path).name 
-            }
-        )
-    return records
-
-def embv(question,records,top_k):
-    qstn=olm.embed(
-        model="embeddinggemma",
-        input=question
-    )
-
-    qstn_arr=np.array(qstn["embeddings"][0])
-    text=[]
-
-    for i in records:
-        text.append(i["text"])
-
-    rec_embed=olm.embed(
-        input=text,
-        model="embeddinggemma"
-    )
-
-    rec_arr=np.array(rec_embed["embeddings"])
-
-    scores= rec_arr @ qstn_arr # Returns list of the vector dot products
-    big_3= np.argsort(scores)[::-1][:top_k]
-
-    context=[]
-
-    for i in big_3:
-        dicti={
-            "text":records[i]["text"],
-            "page":records[i]["page number"],
-            "source":records[i]["source"],
-            "score": float(scores[i])
-        }
-        context.append(dicti)
-    
-    return context
-
-
-"""
 import ollama as olm
 import numpy as np
 from pypdf import PdfReader
@@ -145,6 +74,26 @@ def create_embeddings(records, batch_size=32):
 
     return np.array(vectors)
 
+def load_or_create_embeddings(records, cache_path):
+
+    if Path(cache_path).exists():
+
+        rec_arr = np.load(cache_path)
+
+        if len(rec_arr) == len(records):
+            print("Loaded saved embeddings.")
+            return rec_arr
+
+        print("Document changed. Recreating embeddings...")
+
+
+    rec_arr = create_embeddings(records)
+
+    np.save(cache_path, rec_arr)
+
+    print("Embeddings saved.")
+
+    return rec_arr
 
 def embv(question, records, rec_arr, top_k):
 
